67ceb5906d13c1336d5101498fd7a0f07420af43
Kyle Evans <kevans91@ksu.edu>
2016-11-28 20:38:44 -0600

    Specify universal=1 for bdist_wheel, and add more specific and more general classifiers -- specifically, the Python versions I've tested with and POSIX/POSIX :: BSD because other POSIX OS *could* implement rtprio(2) and at least one unlisted BSD (Dragonfly) does implement rtprio(2)

--- setup.py
+++ setup.py
@@ -12,9 +12,17 @@ setup(
 	classifiers=[
 		'Development Status :: 4 - Beta',
 		'Intended Audience :: Developers',
+		'License :: OSI Approved',
 		'License :: OSI Approved :: BSD License',
+		'Operating System :: POSIX',
+		'Operating System :: POSIX :: BSD',
 		'Operating System :: POSIX :: BSD :: FreeBSD',
 		'Programming Language :: Python',
+		'Programming Language :: Python :: 2',
+		'Programming Language :: Python :: 2.7',
 		'Programming Language :: Python :: 3',
+		'Programming Language :: Python :: 3.4',
+		'Programming Language :: Python :: 3.5',
+		'Programming Language :: Python :: 3.6',
 	]
 )
