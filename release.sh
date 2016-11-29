25f49e8325f0cae0cc2dca18e32898f6c71cad41
Kyle Evans <kevans91@ksu.edu>
2016-11-28 20:44:01 -0600

    Behave a little better if DISTDIR doesn't create at invocation

--- release.sh
+++ release.sh
@@ -1,13 +1,16 @@
 #!/bin/sh
 
-DISTDIR=`realpath ./dist`
+DISTDIR="./dist"
 VERSION=`python3 setup.py --version`
 
 echo "Creating release for ${VERSION}"
 
 rm -r ${DISTDIR}
+mkdir -p ${DISTDIR}
+DISTDIR=`realpath ${DISTDIR}`
 python3 setup.py sdist bdist_wheel
 
+echo "Signing everything in ${DISTDIR}"
 for _file in `ls -1 ${DISTDIR}`; do
 	(cd ${DISTDIR} && gpg --detach-sign -a "${_file}")
 done
