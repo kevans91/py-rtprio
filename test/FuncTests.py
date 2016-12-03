619a266f069bf05745b24d4bf1f5bb1118518248
Kyle Evans <kevans91@ksu.edu>
2016-12-02 23:46:58 -0600

    Tests: Style Concerns

--- test/FuncTests.py
+++ test/FuncTests.py
@@ -1,56 +1,56 @@
 from __future__ import absolute_import, unicode_literals
 
 import unittest
-import rtprio
 from os import getpid, getuid
+import rtprio
 #import sysctl
 
 class FuncTests(unittest.TestCase):
-	def call_rtprio(self, **kwargs):
-		try:
-			return rtprio.rtprio(**kwargs)
-		except Exception as exc:
-			self.fail(exc)
-	
-	def test_get_own(self):
-		mine = self.call_rtprio()
+    def call_rtprio(self, **kwargs):
+        try:
+            return rtprio.rtprio(**kwargs)
+        except Exception as exc:
+            self.fail(exc)
 
-		self.assertEqual(mine[0], 'normal')
+    def test_get_own(self):
+        mine = self.call_rtprio()
 
-			# This /could/ fail, but it is unlikely
-			# The system scheduler would have to bump
-			# our priority in between these two lookups
-		also_mine = self.call_rtprio(pid=getpid())
-		self.assertEqual(mine, also_mine)
+        self.assertEqual(mine[0], 'normal')
 
-	def test_get_other(self):
-			# Get priority of init -
-		other = self.call_rtprio(pid=1)
+            # This /could/ fail, but it is unlikely
+            # The system scheduler would have to bump
+            # our priority in between these two lookups
+        also_mine = self.call_rtprio(pid=getpid())
+        self.assertEqual(mine, also_mine)
 
-	def set_mine(self):
-		base_priority = 6
-		priority = base_priority
-		kern_ignores_prioval = ['normal']
+    def test_get_other(self):
+            # Get priority of init -
+        self.call_rtprio(pid=1)
 
-		for rtprio_type in rtprio.types:
-				# Try changing it to the type requested
-			res = self.call_rtprio(type=rtprio_type, prio=priority)
-			self.assertEqual(res, (rtprio_type, priority))
+    def set_mine(self):
+        base_priority = 6
+        priority = base_priority
+        kern_ignores_prioval = ['normal']
 
-				# Verify that it's changed
-			cur_priority = self.call_rtprio()
+        for rtprio_type in rtprio.types:
+                # Try changing it to the type requested
+            res = self.call_rtprio(priotype=rtprio_type, prio=priority)
+            self.assertEqual(res, (rtprio_type, priority))
 
-			if rtprio_type not in kern_ignores_prioval:
-				self.assertEqual(res, cur_priority)
-			else:
-				self.assertEqual(res[0], cur_priority[0])
+                # Verify that it's changed
+            cur_priority = self.call_rtprio()
 
-			priority += 1	
+            if rtprio_type not in kern_ignores_prioval:
+                self.assertEqual(res, cur_priority)
+            else:
+                self.assertEqual(res[0], cur_priority[0])
 
-	@unittest.skipIf(getuid() != 0, 'Set tests must be ran as root')
-	def test_privileged_set_mine(self):
-		self.set_mine()
+            priority += 1
+
+    @unittest.skipIf(getuid() != 0, 'Set tests must be ran as root')
+    def test_privileged_set_mine(self):
+        self.set_mine()
 
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
--- test/ImplTests.py
+++ test/ImplTests.py
@@ -4,11 +4,11 @@ import unittest
 import rtprio
 
 class ImplTests(unittest.TestCase):
-	def test_libc(self):
-		self.assertIsNotNone(rtprio.libc)	
+    def test_libc(self):
+        self.assertIsNotNone(rtprio.libc)
 
-	def test_exists(self):
-		self.assertTrue(rtprio.rtprio_exists())
+    def test_exists(self):
+        self.assertTrue(rtprio.rtprio_exists())
 
 if __name__ == '__main__':
-	unittest.main()
+    unittest.main()
